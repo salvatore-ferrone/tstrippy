59c73038a54abe688f514e67784072625fa29a6a
Salvatore <salvatore.ferrone@obspm.fr>
2024-01-09 16:00:46 +0100

    adding assert statements to check for proper inputs and that file exists

--- tstrippy/dataParsers/baumgardtMWGCs.py
+++ tstrippy/dataParsers/baumgardtMWGCs.py
@@ -1,8 +1,9 @@
 import astropy.units as u
+import astropy.coordinates as coord
 from astropy.io import fits
 import numpy as np
 import pkg_resources
-
+import os
 
 class baumgardtMWGCs:
     """
@@ -10,10 +11,12 @@ class baumgardtMWGCs:
 
     This class provides methods to extract data from a FITS file, select a globular cluster of interest, 
     and build a covariance matrix of the observed kinematics of a given globular cluster.
+    
+    This path will store all the data, and return an astropy object of all the data in ICRS coordinates.
 
     Attributes
     ----------
-    pathtoclusterdata : str
+    _pathtoclusterdata : str
         The path to the FITS file containing the cluster data.
     data : dict
         The data extracted from the FITS file.
@@ -71,22 +74,59 @@ class baumgardtMWGCs:
             "Mass":u.Msun,
             "rh_m":u.kpc,
             "DM":u.Msun,
-            "rhopmrade":""}
+            "rhopmrade":u.dimensionless_unscaled}
         ):
         """ extracts the data from the fits file and stores it in a dictionary
 
         """        
         
-        self.pathtoclusterdata=pkg_resources.resource_filename('tstrippy', 'data/2023-03-28-merged.fits')
+        # CHECK THAT THE FILE EXISTS
+        self._pathtoclusterdata=pkg_resources.resource_filename('tstrippy', 'data/2023-03-28-merged.fits')
+        self._unitkeys = ["RA","DEC","Rsun","RV","mualpha","mu_delta","Mass","rh_m","DM","rhopmrade"]
+        self._unittypes= ["angle","angle","length","velocity","velocity","velocity","mass","length","length",""]
         
-        self._extractdatafromfits()
+        assert self._pathtoclusterdata is not None, "The path to the cluster data is not valid"
+        assert isinstance(self._pathtoclusterdata,str), "The path to the cluster data is not a string"
+        if not os.path.exists(self._pathtoclusterdata):
+            raise ValueError("The path to the cluster data is not valid")
+        
+        # CHECK THAT THE UNITS ARE VALID
+        for key in units.keys():
+            assert key in self._unitkeys, f"{key} is not a valid for the units dictionary"
+        for key in self._unitkeys:
+            assert key in units.keys(), f"{key} is not present in the units dictionary"
+
+        # check that there are units present
+        assert isinstance(units['RA'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['DEC'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['Rsun'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['RV'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['mualpha'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['mu_delta'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['Mass'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['rh_m'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        assert isinstance(units['DM'],u.UnitBase), f"{units[key]} is not a valid astropy unit"
+        
+        # check that the are the correct units
+        assert units['RA'].is_equivalent(u.degree), f"The units of RA is {units['RA']}, which is not equvalent to degree"
+        assert units['DEC'].is_equivalent(u.degree), f"The units of DEC is {units['DEC']}, which is not equvalent to degree"
+        assert units['Rsun'].is_equivalent(u.kpc), f"The units of Rsun is {units['Rsun']}, which is not equvalent to kpc"
+        assert units['RV'].is_equivalent(u.km/u.s), f"The units of RV is {units['RV']}, which is not equvalent to km/s"
+        assert units['mualpha'].is_equivalent(u.mas/u.yr), f"The units of mualpha is {units['mualpha']}, which is not equvalent to mas/yr"
+        assert units['mu_delta'].is_equivalent(u.mas/u.yr), f"The units of mu_delta is {units['mu_delta']}, which is not equvalent to mas/yr"
+        assert units['Mass'].is_equivalent(u.Msun), f"The units of Mass is {units['Mass']}, which is not equvalent to Msun"
+        assert units['rh_m'].is_equivalent(u.kpc), f"The units of rh_m is {units['rh_m']}, which is not equvalent to kpc"
+        assert units['DM'].is_equivalent(u.Msun), f"The units of DM is {units['DM']}, which is not equvalent to Msun"
+        assert units['rhopmrade'].is_equivalent(u.dimensionless_unscaled), f"The units of rhopmrade is {units['rhopmrade']}, which is not equvalent to dimensionless_unscaled"
+
         self.units=units
+        self._extractdatafromfits()
 
 
     def _extractdatafromfits(self):
         """ internal function to extract the data from the fits file and store it in a dictionary
         """        
-        GCfits=fits.open(self.pathtoclusterdata)
+        GCfits=fits.open(self._pathtoclusterdata)
         fields = [GCfits[1].header['TTYPE'+str(i)] for i in range(1,GCfits[1].header['TFIELDS']+1)]
         units = [GCfits[1].header['TUNIT'+str(i)] for i in range(1,GCfits[1].header['TFIELDS']+1)]
         data={}

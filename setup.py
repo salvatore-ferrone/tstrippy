14bc562f6f2ea1ea36f8badd8fe37effe2771587
sferrone <salvatore.ferrone@obspm.fr>
2024-07-04 11:12:26 +0200

    modified following co-pilots advice

--- setup.py
+++ setup.py
@@ -1,34 +1,21 @@
 from numpy.distutils.core import setup, Extension
 import setuptools
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-# my fortran module
-
-
 constants_module = Extension('tstrippy.lib.constants',
-                              sources=[
-                                  'tstrippy/src/constants.f90',
-                                  ])
+                             sources=['tstrippy/src/constants.f90'])
 
 potentials_module = Extension('tstrippy.lib.potentials',
-                              sources=[
-                                  'tstrippy/src/constants.f90',
-                                  'tstrippy/src/potentials.f90',
-                                  ])
+                              sources=['tstrippy/src/potentials.f90'])
 
 integrator_module = Extension('tstrippy.lib.integrator',
-                              sources=[
-                                  'tstrippy/src/constants.f90',
-                                  'tstrippy/src/potentials.f90',
-                                  'tstrippy/src/galacticbar.f90',
-                                  'tstrippy/src/perturbers.f90',
-                                  'tstrippy/src/hostperturber.f90',
-                                  'tstrippy/src/integrator.f90', 
-                                  ])
-
-
+                              sources=['tstrippy/src/galacticbar.f90',
+                                       'tstrippy/src/perturbers.f90',
+                                       'tstrippy/src/hostperturber.f90',
+                                       'tstrippy/src/integrator.f90'])
 
 setup(
     name="tstrippy",
@@ -41,15 +28,19 @@ setup(
     url="https://github.com/salvatore-ferrone",
     packages=setuptools.find_packages(include=['tstrippy', 'tstrippy.*']),
     classifiers=[
-        "Programming Language :: Python :: 3.11.2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
-        "Operating System :: Mac only (for now)",
+        "Operating System :: MacOS :: MacOS X",
     ],
     python_requires='>=3.6',
-    install_requires=['numpy',
-                      "h5py",
-                      'astropy',],
-    ext_modules=[constants_module,potentials_module,integrator_module],
+    install_requires=['numpy', 'h5py', 'astropy'],
+    ext_modules=[constants_module, potentials_module, integrator_module],
     package_data={'tstrippy': ['data/*']},
 )

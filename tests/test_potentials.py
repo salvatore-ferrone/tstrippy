2441591f8a92d6a1950ef128b9c1743bcc1a55e6
Salvatore <salvatore.ferrone@obspm.fr>
2024-10-24 14:23:11 +0200

    juse parsers, no more data parsers

--- tests/test_potentials.py
+++ tests/test_potentials.py
@@ -1,7 +1,7 @@
 # tstrippy/tests/test_potentials.py
 import tstrippy
 import tstrippy.lib 
-import tstrippy.dataParsers
+import tstrippy.Parsers
 
 
 def test_instantiation():
@@ -10,13 +10,13 @@ def test_instantiation():
 
 
 def test_obtaining_parameters():
-    MWparams = tstrippy.dataParsers.potential_parameters.pouliasis2017pii()
+    MWparams = tstrippy.Parsers.potential_parameters.pouliasis2017pii()
     assert len(MWparams) == 11
     
     return None
 
 def test_pouliasis2017pii():
-    MWparams = tstrippy.dataParsers.potential_parameters.pouliasis2017pii()
+    MWparams = tstrippy.Parsers.potential_parameters.pouliasis2017pii()
     x,y,z = 1,2,3
     out=tstrippy.lib.potentials.pouliasis2017pii(MWparams,x,y,z)
     assert(len(out)==4)

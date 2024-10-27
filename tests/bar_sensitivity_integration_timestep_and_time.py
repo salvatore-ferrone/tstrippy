9c3268dfd37fd9bbc13e52649848a55dae6299bc
Salvatore <salvatore.ferrone@obspm.fr>
2024-10-27 20:07:20 +0100

    added plotting function to show both orbits

--- tests/bar_sensitivity_integration_timestep_and_time.py
+++ tests/bar_sensitivity_integration_timestep_and_time.py
@@ -5,6 +5,7 @@ import astropy.coordinates as coord
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+import matplotlib.colors as mcolors
 
 
 
@@ -20,37 +21,84 @@ def main(targetGC):
     # Reduce the mass of the disks to account for the bar
     MWparams[5] = 1120.0 * 2.32*10**7 
     MWparams[8] = 1190.0 * 2.32*10**7 
-
+    # Extract the initial conditions of the target globular cluster
     x0,y0,z0,vx0,vy0,vz0 = pick_globular_cluster(targetGC, MWrefframe)
+
+    #### Begin storing the input data for the integrator
     Galaxy = ["pouliasis2017pii", MWparams]
     bar = ["longmuralibar", barparams, barpolycoeff]
     initialkinematics = [x0,y0,z0,vx0,vy0,vz0]
     # Pick the target globular cluster
-    
     integrationtime = 1e9
     timestep = 1e7
-    backward_and_forward_orbit(integrationtime,timestep)
+    integrationparameters = [integrationtime,timestep]
+    backward_and_forward_orbit(integrationtime,timestep,Galaxy,initialkinematics,bar)
     
     return None
 
 
-def backward_and_forward_orbit(integartionparams,integrationparameters,staticgalaxy,initialkinematics,galacticbar):
+def backward_and_forward_orbit(integrationtime,timestep,staticgalaxy,initialkinematics,galacticbar):
     
-    integrationtime,dt,Ntimestep=integrationparameters
 
-    assert integrationTime > 0
+    assert integrationtime > 0
     assert timestep > 0
-    assert isinstance(integrationTime, (int, float))
+    assert isinstance(integrationtime, (int, float))
     assert isinstance(timestep, (int, float))
     unitT, unitV, unitD, unitM, unitG, G = loadunits()
 
-    Ntimestep=int(integrationTime/timestep)
-    T,dt=integrationTime*u.yr,timestep*u.yr
+    # convert to integration units
+    Ntimestep=int(integrationtime/timestep)
+    T,dt=integrationtime*u.yr,timestep*u.yr
     T,dt=T.to(unitT),dt.to(unitT)
+    # make sure the current time is set to today for backward integration
     currenttime = 0*unitT
-
+    integrationparameters = [currenttime,dt,Ntimestep]
     xBackward,yBackward,zBackward,vxBackward,vyBackward,vzBackward = \
-        backward_orbit([currenttime,dt,Ntimestep],MWparams,[x0,y0,z0,vx0,vy0,vz0],[barname,barparams,barpolycoeff])
+        backward_orbit(integrationparameters,staticgalaxy,initialkinematics,galacticbar)
+    
+
+    print("len(xBackward)",len(xBackward),)
+    plotdata0={"x":xBackward[0],"y":yBackward[0]}
+    pltobj0={"label":"Backward orbit","color":"blue"}
+
+    plot_orbits([plotdata0],[pltobj0])
+    return None
+
+
+def plot_orbits(plotData,plotProperties,axisconfig={
+        'xlabel':"X [kpc]",
+        'ylabel':"Y [kpc]",
+        'aspect':'equal',},
+        legendconfig={'loc':'center left', 'bbox_to_anchor':(1.05, 0.5)},
+        ):
+
+    assert isinstance(plotData, list)
+    assert isinstance(plotProperties, list)
+    assert isinstance(axisconfig, dict)
+    assert len(plotData) == len(plotProperties)
+
+    nplots = len(plotData)
+    scattersizes = np.logspace(2,0,nplots)
+    linewidths = np.linspace(4,0.5,nplots)
+    fig,axis=plt.subplots(1,1,figsize=(6,5))
+    for i in range(nplots):
+        plotProperties[i]['linewidth']=linewidths[i]
+        plotProperties[i]['zorder']=nplots-i
+        axis.plot(plotData[i]['x'],plotData[i]['y'],**plotProperties[i])
+
+
+    cmap = plt.get_cmap('tab10') 
+    for i in range(nplots):
+        mylabel = plotProperties[i].get('label','')
+        axis.scatter(plotData[i]['x'][0],plotData[i]['y'][0],s=scattersizes[i],label=mylabel+" Initial",color=plotProperties[i]['color'])
+        mycolor = cmap(i % cmap.N)
+        axis.scatter(plotData[i]['x'][-1],plotData[i]['y'][-1],s=scattersizes[i],label=mylabel+" Final",color=mycolor)
+
+    axis.legend(**legendconfig)
+    axis.set(**axisconfig)
+    fig.tight_layout()
+    fig.savefig("orbit_integration_timestep_and_time.png")
+
 
 
 def backward_orbit(integrationparameters,staticgalaxy,initialkinematics,galacticbar):
@@ -63,8 +111,8 @@ def backward_orbit(integrationparameters,staticgalaxy,initialkinematics,galactic
     tstrippy.integrator.setstaticgalaxy(MWname,MWparams)
     tstrippy.integrator.setbackwardorbit()
     tstrippy.integrator.setintegrationparameters(currenttime.value,dt.value,Ntimestep)
-    tstrippy.integrator.setinitialconditions(x0,y0,z0,vx0,vy0,vz0)
-    tstrippy.integrator.setgalacticbar(barname,barparams,barpolycoeff)
+    tstrippy.integrator.setinitialkinematics(x0,y0,z0,vx0,vy0,vz0)
+    tstrippy.integrator.initgalacticbar(barname,barparams,barpolycoeff)
     starttime = datetime.datetime.now()
     xBackward,yBackward,zBackward,vxBackward,vyBackward,vzBackward=tstrippy.integrator.leapfrogintime(Ntimestep,nObj)
     endtime=datetime.datetime.now()

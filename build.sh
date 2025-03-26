1a957d1851516bc7a182a935f9e0424537eb7750
salvatore-ferrone <salvatore.ferrone.1996@gmail.com>
2025-03-26 20:02:43 +0000

    added script for a quick build

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
-#!
+#!/bin/bash
+
+# filepath: /home-filer/sferrone/tstrippy/build.sh
+# Build and install the package
+
+
+# Clean previous builds
 rm -rf builddir
-meson setup builddir 
-meson compile -C builddir/
+
+# Configure
+meson setup builddir
+
+# Build
+meson compile -C builddir
+
+# install 
 meson install  -C builddir/
